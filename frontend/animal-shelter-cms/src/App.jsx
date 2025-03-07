fce77a1da4bb6915861274f2f442cc3199411a83
johnilluminati <johnbilluminati@gmail.com>
2025-03-07 03:02:16 -0500

    Home, Login, & SignUp routes and pages added to react project

--- frontend/animal-shelter-cms/src/App.jsx
+++ frontend/animal-shelter-cms/src/App.jsx
@@ -1,14 +1,19 @@
+import { Router, Routes, Route } from 'react-router-dom';
+
+const routes = (
+  <Router>
+    <Routes>
+      <Route path='/' exact element={<Home />} />
+      <Route path='/login' exact element={<Login />} />
+      <Route path='/signup' exact element={<SignUp />} />
+    </Routes>
+  </Router>
+);
+
 function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-  return ({});
+    <div>{ routes }</div>
+  )
 }
 
 export default App
